3f4ce947209e74b19e3cd03b40dbbf199f2bf13b
Dhanunjay <dhanunjaydraksharapu@gmail.com>
2023-12-28 12:09:44 +0530

    BubbleSort & SelectionSort Completed

---
+++ code/src/com/dhanunjay/Arrays/Sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+package com.dhanunjay.Arrays.Sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter elements of array :");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            int maxIndex = 0;
+            for(int j = 0; j < n - i; j++){
+                if(arr[maxIndex] < arr[j]){
+                    maxIndex = j;
+                }
+            }
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
