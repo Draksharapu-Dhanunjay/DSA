0b727018490ef9bb84fabf47ab0285a9d71a8ddf
Dhanunjay <dhanunjaydraksharapu@gmail.com>
2023-12-28 12:27:52 +0530

    InsertionSort Completed

---
+++ code/src/com/dhanunjay/Arrays/Sorting/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.dhanunjay.Arrays.Sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter elements of array :");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j > 0; j--){
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j -1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
