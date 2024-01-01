e655498a121ea2ca9b0e0587603d25f8e35bfa52
Dhanunjay <dhanunjaydraksharapu@gmail.com>
2024-01-01 18:38:13 +0530

    MaximumConsecutiveOnes Completed!

---
+++ code/src/com/dhanunjay/Arrays/Basics/MaximumConsecutiveOnes.java
@@ -0,0 +1,17 @@
+package com.dhanunjay.Arrays.Basics;
+
+public class MaximumConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,0,1,0,1,0};
+        int count = 0, len = 0;
+        for (int j : arr) {
+            if (j == 1) {
+                count++;
+                len = Math.max(len, count);
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println("Number of Consecutive One's" + len);
+    }
+}
